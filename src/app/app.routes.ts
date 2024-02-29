4ecbfa65bbe8f174208218da5a9af45b7ce2464a
enrique <jesusen10a@gmail.com>
2024-02-29 10:38:51 -0700

    configuracion para nuestro router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { PrimerDiaComponent } from './primer-dia/primer-dia.component';
+import { SegundoDiaComponent } from './segundo-dia/segundo-dia.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'primer-dia',
+        component: PrimerDiaComponent
+    },
+    {
+        path: 'primer-dia',
+        component: PrimerDiaComponent
+    },
+    {
+        path: 'segundo-dia',
+        component: SegundoDiaComponent
+    }
+];
